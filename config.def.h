20c05c2b8c62d79bc4cf7086a0c2321637168e46
Marko Bauhardt <mb@datameer.com>
2022-07-22 09:31:12 +0200

    use dmenu_run_history instead dmenu_run

--- config.def.h
+++ config.def.h
@@ -62,7 +62,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, NULL };
+static const char *dmenucmd[] = { "dmenu_run_history", "-m", dmenumon, "-fn", dmenufont, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *upvol[]   = { "sndioctl", "output.level=+0.05", NULL };
 static const char *downvol[] = { "sndioctl", "output.level=-0.05", NULL };
